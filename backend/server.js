37f2d85cb142b70e91b734d52c975228b1b248dd
ZakariaBAtty <zbatty0912@gmail.com>
2021-04-11 11:26:26 +0000

    completed backend

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,20 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+// require all router
+const client = require('./app/routers/client.router');
+const owner = require('./app/routers/owner.router');
+
 const app = express();
 // middlewers
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// router
+app.use('/', client);
+app.use('/', owner);
 
 // connection
 require('./app/config/database/connection');
